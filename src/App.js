c108a2e52876d71b6e807029a9bc1260e82348ff
David Gabriel <contact@david-gabriel.com>
2026-08-17 19:54:23 -0700

    Remove dead page files and unused routes for demos and blog

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
 import LandingPage from './pages/LandingPage';
-import DemosPage from './pages/DemosPage';
-import BlogPage from './pages/BlogPage';
 import HireMePage from './pages/HireMePage';
 import JigsawPuzzlePage from './pages/JigsawPuzzlePage';
 import CopilotWidget from './components/CopilotWidget';
@@ -14,8 +12,6 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/demos" element={<DemosPage />} />
-          <Route path="/blog" element={<BlogPage />} />
           <Route path="/hire-me" element={<HireMePage />} />
           <Route path="/demos/jigsaw-puzzle" element={<JigsawPuzzlePage />} />
         </Route>
